98030459d7c2cc07c731c1a1f46dd41b3bcc3c1e
Faranak Nasoori <77094327+FaranakNS@users.noreply.github.com>
2024-05-03 11:45:22 -0600

    Add files via upload

---
+++ ReceivingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ReceivingSystem.BLL;
+using ReceivingSystem.DAL;
+
+namespace ReceivingSystem
+{
+    public static class ReceivingExtensions
+    {
+        public static void ReceivingSystemDependencies(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            services.AddDbContext<EbikeContext>(options);
+
+            services.AddTransient(serviceProvider =>
+            {
+                var context = serviceProvider.GetRequiredService<EbikeContext>();
+                return new OrderServices(context);
+            });
+        }
+    }
+}
---
+++ ServicingExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ServicingSystem.DAL;
+using ServicingSystem.BLL;
+#endregion
+
+namespace ServicingSystem
+{
+    public static class ServicingExtensions
+    {
+        public static void ServicingSystemDependencies
+            (this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            services.AddDbContext<EbikeContext>(options);
+
+            services.AddTransient<CustomerServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetRequiredService<EbikeContext>();
+                return new CustomerServices(context);
+            });
+
+            services.AddTransient<CustomerVehicleServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetRequiredService<EbikeContext>();
+                return new CustomerVehicleServices(context);
+            });
+
+            services.AddTransient<StandardServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetRequiredService<EbikeContext>();
+                return new StandardServices(context);
+            });
+
+            services.AddTransient<CouponServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetRequiredService<EbikeContext>();
+                return new CouponServices(context);
+            });
+
+            services.AddTransient<JobServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetRequiredService<EbikeContext>();
+                return new JobServices(context);
+            });
+
+        }
+    }
+}
